ac2863081b67e8ffb670c5618938188230d897f7
Evilpasture <mandatorythebest@gmail.com>
2026-01-05 11:06:09 +0700

    clean up comment

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ import sys
 import sysconfig
 from setuptools import Extension, setup
 
-# USE_ASAN = os.getenv('USE_ASAN') == '1'
 
 extra_compile_args = []
-# extra_link_args = []
 define_macros = [('Py_GIL_DISABLED', '1')]
 libraries = []
 
@@ -37,33 +35,11 @@ if os.getenv('DISABLE_LOCKS', '0') == '1':
 else:
     print("Building without DEBUG")
 
-# if sys.platform == 'win32':
-#     # Standard flags
-#     extra_compile_args += ['/std:c11', '/O2', '/Zi']
-#     libraries += ['opengl32', 'user32', 'gdi32']
-    
-#     if USE_ASAN:
-#         # ASan specific flags for MSVC
-#         # We must disable /incremental and /ZI (Edit and Continue) as they conflict with ASan
-#         extra_compile_args += ['/fsanitize=address']
-#         extra_link_args = [
-#             '/INCREMENTAL:NO',
-#             '/DEBUG',
-#             '/WHOLEARCHIVE:clang_rt.asan_dynamic-x86_64.lib',
-#             '/WHOLEARCHIVE:clang_rt.asan_dynamic_runtime_thunk-x86_64.lib'
-#         ]
-#         libraries += ['clang_rt.asan_dynamic-x86_64', 'clang_rt.asan_dynamic_runtime_thunk-x86_64']
-#         # Remove conflicting flags if they exist
-#         if '/Zi' in extra_compile_args: extra_compile_args.remove('/Zi')
-#         extra_compile_args.append('/Z7') # Use C7 compatible debug info instead
-#         print("MSVC Building with AddressSanitizer enabled")
-
 ext = Extension(
     name='hypergl._hypergl_c',
     sources=['hypergl.c'],
     libraries=libraries,
     extra_compile_args=extra_compile_args,
-    # extra_link_args=extra_link_args,
     define_macros=define_macros, # type: ignore
 )
 
